24c4525677d7e5fcac02618a3b43994522d3629d
{sharath} <sharathkumarchelimella@gmail.com>
2026-05-07 23:40:43 +0530

    .

---
+++ HashMap/05_duplicates.java
@@ -0,0 +1,17 @@
+//217. Contains Duplicate
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        int n = nums.length;
+        HashMap<Integer, Integer> hash = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            if(hash.containsKey(nums[i])){
+                return true;
+            }else{
+                hash.put(nums[i],1);
+            }   
+        }
+        return false;
+        
+    }
+}
